b3b6c536f1434ec7ed5a4a687d85331ef16f85b1
psu6810110229 <6810110229@psu.ac.th>
2026-02-26 11:40:28 +0700

    resolve merge conflict in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // อนุญาตให้ frontend dev server เรียก API ได้ (แก้ปัญหาเรียกจาก port 5173 ไม่ผ่าน)
   app.enableCors({
@@ -10,7 +13,10 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
