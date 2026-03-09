d2925b0d48ef8e96c74911cb38e1113fb3fce97a
psu6810110347 <6810110347@psu.ac.th>
2026-03-09 14:32:49 +0700

    fix:playwright reuseExistingServer: true

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,13 +11,13 @@ export default defineConfig({
     {
       command: 'npm --prefix backend run start:dev',
       url: 'http://127.0.0.1:3000/',
-      reuseExistingServer: !process.env.CI,
+      reuseExistingServer: true,
       timeout: 120_000,
     },
     {
       command: 'npm --prefix frontend run dev -- --host 127.0.0.1',
       url: 'http://127.0.0.1:5173',
-      reuseExistingServer: !process.env.CI,
+      reuseExistingServer: true,
       timeout: 120_000,
     },
   ],
